ab6c49e4abbb1a4192e072db73d0bb6b030ee03a
jy00088 <118143240+JY00088@users.noreply.github.com>
2023-03-06 21:08:06 +0900

    pm09:08
    03.06

--- clone/src/App.js
+++ clone/src/App.js
@@ -1,11 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useRef } from "react";
+import { Link, BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from './page/Home';
+import './scss/Style.scss'
+import Header from "./page/Header";
 
 function App() {
+  
   return (
-    <div className="App">
-     
-    </div>
+    <BrowserRouter>
+      <Header />
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+        </Routes>
+      </main>
+    </BrowserRouter>
+
   );
 }
 
