75075bb053defc547242cf61bf08554adb8813d4
jy00088 <118143240+JY00088@users.noreply.github.com>
2023-03-13 16:41:21 +0900

    03.13
    pm04:41

--- clone/src/App.js
+++ clone/src/App.js
@@ -1,27 +1,26 @@
-import { useState, useRef } from "react";
-import { Link, BrowserRouter, Route, Routes } from "react-router-dom";
+import { useState, useRef } from 'react';
+import { Link, BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './page/Home';
-import './scss/Style.scss'
-import Header from "./page/Header";
-import Trailer from "./page/Trailer";
-import Mycontext from "./Mycontext";
+import './scss/Style.scss';
+import Header from './page/Header';
+import Trailer from './page/Trailer';
+import MainBox from './page/MainBox';
+import Mycontext from './Mycontext';
 
 function App() {
-  
   return (
     <BrowserRouter>
-    <Mycontext>
-      <Header />
-      <main>
-        <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/trailer" element={<Trailer />}></Route>
-        </Routes>
-        
-      </main>
+      <Mycontext>
+        <Header />
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/trailer" element={<Trailer />}></Route>
+            <Route path="/mainbox" element={<MainBox />}></Route>
+          </Routes>
+        </main>
       </Mycontext>
     </BrowserRouter>
-
   );
 }
 
